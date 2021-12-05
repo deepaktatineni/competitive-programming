23018b55002cbe13bac30d5aa3b154056bfdb989
Deepak <depktatineni@outlook.com>
2021-12-05 22:45:45 +0530

    first commit

---
+++ om.cpp
@@ -0,0 +1,29 @@
+
+// Question: Paste web url for problem statement
+
+// Approach: Mention approach (eg: brute force / optimised)
+
+// Time complexity: O(log n)
+
+// Space complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE 
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+	cin >> n;
+	cout << n << endl;
+
+	return 0;
+
+}
